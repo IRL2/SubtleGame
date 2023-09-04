19148998f042e4456a2536618abe8f870c31a2cf
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2023-09-04 15:31:24 +0200

    Add a method that handles the initialisation of each task
    This method currently only handles the nanotube and knot-tying tasks, not the psychophysics trials yet. This is a step towards creating a handler for the entire game.

--- Client/puppeteering_client.py
+++ Client/puppeteering_client.py
@@ -22,67 +22,82 @@ class PuppeteeringClient:
         self.alanine_index = [idx for idx, s in enumerate(simulations['simulations']) if 'alanine' in s]
         self.buckyball_indices = [idx for idx, s in enumerate(simulations['simulations']) if 'buckyball' in s]
 
-        # Generate order of tasks for game
-        order_of_tasks = get_order_of_tasks()
+        self.order_of_tasks = get_order_of_tasks()
+        self.current_task_index = None
 
         # Generate order of simulations for game
         self.order_of_sims = get_order_of_simulations(nanotube=self.nanotube_index,
                                                       alanine=self.alanine_index,
                                                       trial_indices=self.buckyball_indices,
-                                                      tasks_ordered=order_of_tasks,
+                                                      tasks_ordered=self.order_of_tasks,
                                                       num_repeats=num_trial_repeats)
 
         # Randomise order of interaction mode for each section
-        interaction_modes_randomised = random.sample(['hands', 'controllers'], 2)
-        self.current_interaction_mode = interaction_modes_randomised[0]
+        self.interaction_modes_randomised = random.sample(['hands', 'controllers'], 2)
+        self.current_interaction_mode = self.interaction_modes_randomised[0]
 
         # Write data to state dict
         # TODO: store these here and not the shared state
-        self.narupa_client.set_shared_value('Order of tasks', order_of_tasks)
+        self.narupa_client.set_shared_value('Order of tasks', self.order_of_tasks)
         self.narupa_client.set_shared_value('Order of simulations', self.order_of_sims)
-        self.narupa_client.set_shared_value('Order of interaction mode', interaction_modes_randomised)
+        self.narupa_client.set_shared_value('Order of interaction modes', self.interaction_modes_randomised)
 
-        # Initiate other attributes
         self.knot_pull_client = None
 
-    def start_knot_tying_simulation(self, alanine_sim_index):
-        """ Connects the knot-detection client to the server and continuously checks for a knot. Once a knot is
-        detected, the knot-detection client disconnects from the server and resets the 17-Ala simulation."""
+    def initialise_task(self, task_type: str):
+        """ Handles the initialisation of each task. """
 
-        # Start 17-alanine simulation (must be running for knot-pull to work)
-        self.narupa_client.run_command("playback/load", index=alanine_sim_index)
+        if not self.current_task_index:
+            self.current_task_index = 0
+        else:
+            self.current_task_index += 1
+
+        self.narupa_client.set_shared_value('task', task_type)
+        self.narupa_client.set_shared_value('task status', 'in progress')
+
+        if task_type == 'nanotube':
+            self.narupa_client.set_shared_value('simulation index', self.nanotube_index)
+            self.start_nanotube_practice_task()
+
+        elif task_type == 'knot-tying':
+            self.narupa_client.set_shared_value('simulation index', self.alanine_index)
+            self.start_knot_tying_task()
+
+        else:
+            # TODO: add function for psychophysics trials task
+            pass
+
+        # Check if this was the final task
+        if self.current_task_index == len(self.order_of_tasks) - 1:
+            self.finish_game()
+
+    def start_knot_tying_task(self):
+        """ Starts the knot-tying task where the user attempts to tie a trefoil knot in a 17-alanine polypeptide."""
+
+        self.narupa_client.run_command("playback/load", index=self.alanine_index)
         self.narupa_client.run_command("playback/play")
 
-        # Write to shared state
-        self.narupa_client.set_shared_value('Task B: 17-Ala knot detection', 'started')
-
-        # Create knot-pull client
         self.knot_pull_client = NarupaKnotPullClient()
 
         while True:
-            # Run knot-pull
+
             self.knot_pull_client.check_if_chain_is_knotted()
 
-            # Is the chain currently knotted?
             if self.knot_pull_client.is_currently_knotted:
-                # Update state dictionary
-                self.narupa_client.set_shared_value('Task B: 17-Ala knot detection', 'completed')
-
-                # Wait 3 seconds and then break loop
+                self.narupa_client.set_shared_value('task status', 'completed')
                 time.sleep(3)
                 break
 
-            # Wait for a bit
             time.sleep(1 / 30)
 
-        # Update shared state and reset the simulation
-        self.narupa_client.remove_shared_value('Task B: 17-Ala knot detection')
-        # TODO: remove reset here
-        self.narupa_client.run_command('playback/reset')
-
-        # Disconnect knot-pull client from server and empty it
         self.knot_pull_client.narupa_client.close()
         self.knot_pull_client = None
+
+    def start_nanotube_practice_task(self):
+        pass
+
+    def finish_game(self):
+        pass
 
 
 if __name__ == '__main__':
@@ -108,7 +123,7 @@ if __name__ == '__main__':
     # TODO: create method that's called here to loop through tasks
 
     print("Checking for a knot...")
-    puppeteering_client.start_knot_tying_simulation(puppeteering_client.alanine_index)
+    puppeteering_client.start_knot_tying_task(puppeteering_client.alanine_index)
 
     print("Closing the narupa client.")
     puppeteering_client.narupa_client.close()
