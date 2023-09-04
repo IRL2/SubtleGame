1781a51036e7dff8a0d17952c2998f4c7fcbb8c8
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2023-09-04 16:02:38 +0200

    Remove test for deleted function

--- Client/tests/preparing_game_test.py
+++ Client/tests/preparing_game_test.py
@@ -1,4 +1,4 @@
-from ..preparing_game import get_order_of_simulations, randomise_order_of_trials, get_order_of_tasks
+from ..preparing_game import randomise_order_of_trials, get_order_of_tasks
 import unittest
 
 
@@ -23,31 +23,6 @@ class PreparingGameTest(unittest.TestCase):
         self.assertTrue(len(randomise_order_of_trials(self.trials_example)), 2)
         self.assertEqual(set(randomise_order_of_trials(self.trials_example)), set(self.trials_example))
         self.assertEqual(len(randomise_order_of_trials(self.trials_example)), len(self.trials_example))
-
-    def test_get_order_of_simulations(self):
-
-        self.assertEqual(len(self.order_of_simulations_example), 8)
-
-        for item in self.order_of_simulations_example:
-            self.assertIsNotNone(item)
-
-        with self.assertRaises(Exception):
-            '1' + 1
-            get_order_of_simulations(nanotube=self.nanotube_example, alanine=self.alanine_example,
-                                     trial_indices=[[2, 3], [2]],
-                                     tasks_ordered=self.tasks_example)
-
-        with self.assertRaises(Exception):
-            get_order_of_simulations(nanotube=self.nanotube_example, alanine=self.alanine_example,
-                                     trial_indices=['a', 'b'],
-                                     tasks_ordered=self.tasks_example)
-
-        with self.assertRaises(Exception):
-            get_order_of_simulations(nanotube=self.nanotube_example, alanine=[0],
-                                     trial_indices=[self.trials_example, self.trials_example],
-                                     tasks_ordered=self.tasks_example)
-
-        self.assertEqual(set(self.order_of_simulations_example), {0, 1, 2, 3, 0, 1, 3, 2})
 
 
 if __name__ == '__main__':
