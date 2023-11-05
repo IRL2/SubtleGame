b23263edf2ebabc51de9c46265248cb77c9cc536
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2023-11-05 15:43:36 +0100

    Stop error being thrown when quitting Unity whilst passthrough is enabled

--- Client/vr-client/Assets/NarupaIMD/Oculus Interaction/PassthroughToggle.cs
+++ Client/vr-client/Assets/NarupaIMD/Oculus Interaction/PassthroughToggle.cs
@@ -2,10 +2,29 @@ using UnityEngine;
 
 namespace NarupaIMD.Oculus_Interaction
 {
+    /// <summary>
+    /// Class <c>PassthroughToggle</c> toggles on passthrough when the GameObject is enabled and toggles passthrough off when the GameObject is disabled.
+    /// </summary>
     public class PassthroughToggle : MonoBehaviour
     {
         [SerializeField] private OVRPassthroughLayer passthroughLayer;
         
+        private void Start()
+        {
+            // Subscribe to the application focus events.
+            Application.focusChanged += OnApplicationFocusChanged;
+        }
+
+        private void OnApplicationFocusChanged(bool hasFocus)
+        {
+            OVRManager.instance.isInsightPassthroughEnabled = hasFocus;
+        }
+
+        private void OnDestroy()
+        {
+            // Stops error when Unity is quit whilst passthrough is enabled.
+            Application.focusChanged -= OnApplicationFocusChanged;
+        }
         private void OnEnable()
         {
             // Enable passthrough when the GameObject is enabled
@@ -15,7 +34,10 @@ namespace NarupaIMD.Oculus_Interaction
         private void OnDisable()
         {
             // Disable passthrough when the GameObject is disabled
-            passthroughLayer.enabled = false;
+            if (!passthroughLayer == false)
+            {
+                passthroughLayer.enabled = false;
+            }
         }
     }
 }
