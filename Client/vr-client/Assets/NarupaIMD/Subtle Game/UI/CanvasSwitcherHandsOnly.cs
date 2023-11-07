2683cdda778df1e6b098966df81f1f852dae7ef6
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2023-11-07 17:44:18 +0100

    Pressing hands-only button with controllers makes objects appear on canvas
    Somewhere this functionality broke, so it needed to be fixed.

--- Client/vr-client/Assets/NarupaIMD/Subtle Game/UI/CanvasSwitcherHandsOnly.cs
+++ Client/vr-client/Assets/NarupaIMD/Subtle Game/UI/CanvasSwitcherHandsOnly.cs
@@ -1,8 +1,11 @@
+using UnityEngine;
+
 namespace NarupaIMD.Subtle_Game.UI
 {
     public class CanvasSwitcherHandsOnly: CanvasSwitcher
     {
         private CanvasModifier _canvasModifier;
+
         /// <summary>
         /// Invoke button press only if pressed by the hands (hand tracking). If pressed by controllers, any Game Objects set by a CanvasModifier attached this this GameObject will be set active.
         /// </summary>
@@ -16,7 +19,9 @@ namespace NarupaIMD.Subtle_Game.UI
             }
             
             // Hands are not tracking, check if the canvas needs to be modified.
-            if (_canvasModifier != null)
+            _canvasModifier = gameObject.GetComponent<CanvasModifier>();
+            
+            if (_canvasModifier!= null)
             {
                 // Enable any Game Objects specified in the CanvasModifier.
                 _canvasModifier.SetObjectsActiveOnCanvas();
