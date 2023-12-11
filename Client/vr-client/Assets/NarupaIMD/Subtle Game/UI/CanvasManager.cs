3ca9165c65f08ff03aec6dfe74b2e23d03b09651
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2023-12-11 14:25:28 +0100

    Remove redundant import

--- Client/vr-client/Assets/NarupaIMD/Subtle Game/UI/CanvasManager.cs
+++ Client/vr-client/Assets/NarupaIMD/Subtle Game/UI/CanvasManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using NarupaIMD.Subtle_Game.Logic;
-using UnityEditor.UI;
 using UnityEngine;
 
 namespace NarupaIMD.Subtle_Game.UI
