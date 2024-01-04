fb8bb3defd60de960a36c4b0e4050258537ad215
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-01-04 11:06:47 +0000

    Use latest_frame rather than current_frame for particle positions
    Cannot currently access the current_frame (Jonathan looking into this). Instead, we use the first frame to get the particle names and residue ids, then the latest frame to access the positions of the atoms

--- Client/task_knot_tying.py
+++ Client/task_knot_tying.py
@@ -11,20 +11,22 @@ class KnotTyingTask(Task):
     def __init__(self, client: NarupaImdClient):
         super().__init__(client)
         self.knot_pull_client = None
+        self.particle_names = client.first_frame.particle_names
+        self.residue_ids = client.first_frame.residue_ids
 
     def _run_logic_for_specific_task(self):
         """ Checks for a knot approx. 30 times per second. Uses the Knot Pull program, which is available on GitHub (
         https://github.com/dzarmola/knot_pull)."""
         # Create knot_pull client
-        self.knot_pull_client = KnotPullClient(atomids=self.client.current_frame.particle_names,
-                                               resids=self.client.current_frame.residue_ids,
-                                               atom_positions=self.client.current_frame.particle_positions)
+        self.knot_pull_client = KnotPullClient(atomids=self.particle_names,
+                                               resids=self.residue_ids,
+                                               atom_positions=self.client.latest_frame.particle_positions)
 
         # Keeping checking if chain is knotted.
         while True:
 
             self.knot_pull_client.check_if_chain_is_knotted(
-                atom_positions=self.client.current_frame.particle_positions)
+                atom_positions=self.client.latest_frame.particle_positions)
 
             if self.knot_pull_client.is_currently_knotted:
                 self.client.set_shared_value('task status', 'completed')
