844048fb47fcd37117c0ce7414fe26a70deada3a
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-01-22 17:41:58 +0100

    Update CenteringSimulationBox.cs
    Change the simulation scale to 0.75 for the knot-tying task (leave it at 1 for the other tasks). Shift the player more into the simulation box for this task too, since they were starting too far away from the 17-ala simulation.

--- Client/vr-client/Assets/NarupaIMD/Subtle Game/Visuals/CenteringSimulationBox.cs
+++ Client/vr-client/Assets/NarupaIMD/Subtle Game/Visuals/CenteringSimulationBox.cs
@@ -28,6 +28,10 @@ namespace NarupaIMD.Subtle_Game.Visuals
         /// Previous value of the length of the simulation box along the x-axis in local coordinates.
         /// </summary>
         private float _previousXMagnitude;
+        
+        private float _simulationScale;
+        
+        private SubtleGameManager _subtleGameManager;
 
         /// <summary>
         /// Centers the simulation box in front of the player if the box changes size. Note: we only check the
@@ -38,7 +42,10 @@ namespace NarupaIMD.Subtle_Game.Visuals
         {
             if (!(Math.Abs(simulationBox.xMagnitude - _previousXMagnitude) > 0.1f)) return;
             
+            _subtleGameManager = FindObjectOfType<SubtleGameManager>();
             _previousXMagnitude = simulationBox.xMagnitude;
+            
+            SetSimulationScale();
             CenterSimulationInFrontOfPlayer();
         }
         
@@ -48,12 +55,19 @@ namespace NarupaIMD.Subtle_Game.Visuals
         /// </summary>
         private void CenterSimulationInFrontOfPlayer()
         {
+            float zComponent = 0f;
+
+            if (_subtleGameManager.CurrentTaskType is SubtleGameManager.TaskTypeVal.KnotTying)
+            {
+                zComponent = -simulationBox.yMagnitude * 0.25f;
+            }
+
             // Set translation vector
             var desiredTranslation = transform;
             desiredTranslation.localPosition = new Vector3(
                 -simulationBox.xMagnitude*0.5f,
                 -simulationBox.yMagnitude*0.5f,
-                0
+                zComponent
             ) ;
             
             // Place the origin of the sim box at position of the center eye anchor
@@ -61,6 +75,19 @@ namespace NarupaIMD.Subtle_Game.Visuals
             
             // Translate the simulation so that the center eye anchor is in the center of the xy plane of the sim box
             simulation.Translate(desiredTranslation.position - simulation.position);
+        }
+
+        private void SetSimulationScale()
+        {
+            _simulationScale = _subtleGameManager.CurrentTaskType switch
+            {
+                SubtleGameManager.TaskTypeVal.Nanotube => 1f,
+                SubtleGameManager.TaskTypeVal.KnotTying => 0.75f,
+                SubtleGameManager.TaskTypeVal.Trials => 1f,
+                _ => _simulationScale
+            };
+
+            simulation.transform.localScale = new Vector3(_simulationScale, _simulationScale,_simulationScale);
         }
         
     }
