12b11df994e086bd570ff36d1e6c6701e4041a5a
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-01-23 10:14:35 +0100

    Put buckyballs slightly lower relative to player for trials task

--- Client/vr-client/Assets/NarupaIMD/Subtle Game/Visuals/CenteringSimulationBox.cs
+++ Client/vr-client/Assets/NarupaIMD/Subtle Game/Visuals/CenteringSimulationBox.cs
@@ -1,6 +1,7 @@
 using System;
 using Narupa.Visualisation;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace NarupaIMD.Subtle_Game.Visuals
 {
@@ -31,44 +32,50 @@ namespace NarupaIMD.Subtle_Game.Visuals
         
         private float _simulationScale;
         
-        private SubtleGameManager _subtleGameManager;
+        public SubtleGameManager subtleGameManager;
 
         /// <summary>
-        /// Centers the simulation box in front of the player if the box changes size. Note: we only check the
+        /// Puts the simulation box in front of the player if the box changes size. Note: we only check the
         /// x-magnitude because all of the simulations are square, so if one length changes then all of them will
         /// change.
         /// </summary>
         private void Update()
         {
+            // TODO: put this into an event rather than in Update
             if (!(Math.Abs(simulationBox.xMagnitude - _previousXMagnitude) > 0.1f)) return;
             
-            _subtleGameManager = FindObjectOfType<SubtleGameManager>();
             _previousXMagnitude = simulationBox.xMagnitude;
             
             SetSimulationScale();
-            CenterSimulationInFrontOfPlayer();
+            PutSimulationInFrontOfPlayer();
         }
         
         /// <summary>
-        /// Centers the simulation in front of the player by placing the center of the VR headset at the center of the
-        /// xy plane of the simulation box, facing along the positive z direction.
+        /// Puts the simulation in front of the player. The default is centering the headset on the xy plane of the
+        /// simulation box, but these values are altered for the knot-tying task and the trials.
         /// </summary>
-        private void CenterSimulationInFrontOfPlayer()
+        private void PutSimulationInFrontOfPlayer()
         {
+            // Set default values: centering the player on the xy plane of the simulation box facing the +z direction
+            float xComponent = -simulationBox.xMagnitude * 0.5f;
+            float yComponent = -simulationBox.yMagnitude * 0.5f;
             float zComponent = 0f;
-
-            if (_subtleGameManager.CurrentTaskType is SubtleGameManager.TaskTypeVal.KnotTying)
+            
+            // Alter values for knot-tying and trials tasks
+            switch (subtleGameManager.CurrentTaskType)
             {
-                zComponent = -simulationBox.yMagnitude * 0.25f;
+                case SubtleGameManager.TaskTypeVal.KnotTying:
+                    zComponent = -simulationBox.yMagnitude * 0.25f;
+                    break;
+                case SubtleGameManager.TaskTypeVal.Trials:
+                    yComponent = -simulationBox.yMagnitude * 0.7f;
+                    zComponent = -simulationBox.yMagnitude * 0.15f;
+                    break;
             }
 
             // Set translation vector
             var desiredTranslation = transform;
-            desiredTranslation.localPosition = new Vector3(
-                -simulationBox.xMagnitude*0.5f,
-                -simulationBox.yMagnitude*0.5f,
-                zComponent
-            ) ;
+            desiredTranslation.localPosition = new Vector3(xComponent, yComponent, zComponent);
             
             // Place the origin of the sim box at position of the center eye anchor
             simulation.position = centreEyeAnchor.position;
@@ -76,17 +83,23 @@ namespace NarupaIMD.Subtle_Game.Visuals
             // Translate the simulation so that the center eye anchor is in the center of the xy plane of the sim box
             simulation.Translate(desiredTranslation.position - simulation.position);
         }
-
+        
+        /// <summary>
+        /// Sets the scale of the simulation. Default value is 1 (nanotube and trials tasks), with a smaller scale of
+        /// 0.75 for the knot-tying task.
+        /// </summary>
         private void SetSimulationScale()
         {
-            _simulationScale = _subtleGameManager.CurrentTaskType switch
+            // Get simulation scale value for each task
+            _simulationScale = subtleGameManager.CurrentTaskType switch
             {
                 SubtleGameManager.TaskTypeVal.Nanotube => 1f,
                 SubtleGameManager.TaskTypeVal.KnotTying => 0.75f,
                 SubtleGameManager.TaskTypeVal.Trials => 1f,
                 _ => _simulationScale
             };
-
+            
+            // Set the scale of the simulation game object
             simulation.transform.localScale = new Vector3(_simulationScale, _simulationScale,_simulationScale);
         }
         
