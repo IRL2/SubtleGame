ca9ce6d45de419b24190f8561cfefc856a8b93d8
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-01-24 17:00:59 +0100

    Add logic for checking that the keys/values are permitted
    The logic needed updating, e.g. for values that are lists rather than single items.

--- Client/additional_functions.py
+++ Client/additional_functions.py
@@ -2,13 +2,10 @@ from narupa.app import NarupaImdClient
 from standardised_values import *
 
 
-def write_to_shared_state(client: NarupaImdClient, key: str, value):
+def write_to_shared_state(client: NarupaImdClient, key: str, value: str):
     """ Writes a key-value pair to the shared state with the puppeteer client namespace. """
 
     check_that_key_val_pair_is_valid(key=key, val=value)
-
-    if not isinstance(key, str):
-        key = str(key)
 
     formatted_key = "puppeteer." + key
     client.set_shared_value(formatted_key, value)
@@ -17,9 +14,24 @@ def write_to_shared_state(client: NarupaImdClient, key: str, value):
 def check_that_key_val_pair_is_valid(key: str, val: str):
     """ Checks if the key-value pair is permitted for writing to the shared state. """
 
+    # Only check values of keys that require specific values
+    if key in keys_with_unrestricted_vals:
+        return
+
+    # Check if key is permitted
     if key not in shared_state_keys_and_vals:
         raise NameError(f"Invalid shared state key '{key}', it must be one of "f"{shared_state_keys_and_vals.keys()}")
 
+    # Where the val is a list, check each item in the list
+    if isinstance(val, list):
+        for i in range(len(val)):
+            if val[i] not in shared_state_keys_and_vals[key]:
+                raise NameError(f"Invalid shared state value '{val[i]}' for key '{key}', it must be one of: "
+                                f"{shared_state_keys_and_vals[key]}")
+            else:
+                return
+
+    # Otherwise, check the value directly
     if val not in shared_state_keys_and_vals[key]:
         raise NameError(f"Invalid shared state value '{val}' for key '{key}', it must be one of: "
                         f"{shared_state_keys_and_vals[key]}")
--- Client/standardised_values.py
+++ Client/standardised_values.py
@@ -11,6 +11,7 @@ task_order = 'order-of-tasks'
 sim_name_nanotube = 'nanotube'
 sim_name_knot_tying = '17-ala'
 sim_name_trials = 'buckyball'
+
 # Interaction modality
 modality_hands = 'hands'
 modality_controllers = 'controllers'
@@ -19,9 +20,12 @@ modality_controllers = 'controllers'
 shared_state_keys_and_vals = {
     'modality': [modality_hands, modality_controllers],
     'game-status': ['waiting', 'finished'], #TODO: add in progress
+    'order-of-tasks': [task_practice, task_knot_tying, task_trials],
     'current-task': [task_practice, task_knot_tying, task_trials],
     'task-status': ['ready', 'in-progress', 'finished'],
-    'task-completion-time': [None],
     'trials-timer': ['started', 'finished'],
-    'trials-answer': [None, True, False]
+    'trials-answer': ['None', 'True', 'False']
 }
+
+keys_with_unrestricted_vals = ['task-completion-time']
+
