c1e7b1a75359d690da03438987f1eb1db0bc549a
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-01-24 16:40:10 +0100

    Move standardised values to separate script

--- Client/puppeteering_client.py
+++ Client/puppeteering_client.py
@@ -3,36 +3,8 @@ from task_nanotube import NanotubeTask
 from task_knot_tying import KnotTyingTask
 from task_trials import TrialsTask
 from additional_functions import write_to_shared_state
+from standardised_values import *
 import random
-
-# Server
-server_name = 'SubtleGame'
-
-# Task
-task_practice = 'nanotube'
-task_knot_tying = 'knot-tying'
-task_trials = 'trials'
-task_order = 'order-of-tasks'
-
-# Simulation
-sim_name_nanotube = 'nanotube'
-sim_name_knot_tying = '17-ala'
-sim_name_trials = 'buckyball'
-
-# Interaction modality
-modality_hands = 'hands'
-modality_controllers = 'controllers'
-
-# Shared state
-shared_state_keys_and_vals = {
-    'modality': [modality_hands, modality_controllers],
-    'game-status': ['waiting', 'finished'], #TODO: add in progress
-    'current-task': [task_practice, task_knot_tying, task_trials],
-    'task-status': ['ready', 'in-progress', 'finished'],
-    'task-completion-time': [None],
-    'trials-timer': ['started', 'finished'],
-    'trials-answer': [None, True, False]
-}
 
 
 def randomise_order(lst: list):
