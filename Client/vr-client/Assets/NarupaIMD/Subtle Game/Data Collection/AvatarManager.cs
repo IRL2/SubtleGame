2041dd622c7db7652c58432aadc4f7c1cac44ebd
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-02-01 09:58:08 +0100

    Avatar data written to shared state successfully

--- Client/vr-client/Assets/NarupaIMD/Subtle Game/Data Collection/AvatarManager.cs
+++ Client/vr-client/Assets/NarupaIMD/Subtle Game/Data Collection/AvatarManager.cs
@@ -14,47 +14,33 @@ namespace NarupaIMD.Subtle_Game.Data_Collection
         private Transformation _leftHand;
         private Transformation _rightHand;
         private Transformation _headset;
-
-        private void Start()
-        {
-            _headset = new Transformation(
-                centerEyeAnchor.position,
-                centerEyeAnchor.rotation, 
-                centerEyeAnchor.localScale
-                     );
-            
-            _leftHand = new Transformation(
-                leftHandAnchor.position,
-                leftHandAnchor.rotation, 
-                leftHandAnchor.localScale
-            );
-            
-            _rightHand = new Transformation(
-                rightHandAnchor.position,
-                rightHandAnchor.rotation, 
-                rightHandAnchor.localScale
-            );
-        }
-
+        
+        /// <summary>
+        /// Updates the local avatar data and flushes this to the shared state.
+        /// </summary>
         private void Update()
         {
-            // Update positions and rotations
-            _headset.Position = centerEyeAnchor.position;
-            _headset.Rotation = centerEyeAnchor.rotation;
-            _headset.Scale = centerEyeAnchor.localScale;
+            if (!simulation.Multiplayer.IsOpen) return;
             
-            _leftHand.Position = leftHandAnchor.position;
-            _leftHand.Rotation = leftHandAnchor.rotation;
-            _leftHand.Scale = leftHandAnchor.localScale;
+            _headset = UpdateTransformation(_headset, centerEyeAnchor);
+            _leftHand = UpdateTransformation(_leftHand, leftHandAnchor);
+            _rightHand = UpdateTransformation(_rightHand, rightHandAnchor);
 
-            _rightHand.Position = rightHandAnchor.position;
-            _rightHand.Rotation = rightHandAnchor.rotation;
-            _rightHand.Scale = rightHandAnchor.localScale;
-            
-            // Update local avatar
             simulation.Multiplayer.Avatars.LocalAvatar.SetTransformations(_headset, _leftHand, _rightHand);
+            simulation.Multiplayer.Avatars.LocalAvatar.Name = "player";
             simulation.Multiplayer.Avatars.FlushLocalAvatar();
-            
+        }
+        
+        /// <summary>
+        /// Updates a <see cref="Transformation"/> with the position, rotation and local scale of the
+        /// <see cref="Transform"/>.
+        /// </summary>
+        private static Transformation UpdateTransformation(Transformation transformation, Transform objectTransform)
+        {
+            transformation.Position = objectTransform.position;
+            transformation.Rotation = objectTransform.rotation;
+            transformation.Scale = objectTransform.localScale;
+            return transformation;
         }
     }
 }
