fdfa472cddc56773bc062b3419346d86205ca275
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-02-02 12:12:29 +0100

    Reduce lifetime of confetti

--- Client/vr-client/Assets/NarupaIMD/Subtle Game/UI/Confetti.cs
+++ Client/vr-client/Assets/NarupaIMD/Subtle Game/UI/Confetti.cs
@@ -23,7 +23,7 @@ namespace NarupaIMD.Subtle_Game.UI
             yield return new WaitForSeconds(2f);
             
             confetti.Stop(false, ParticleSystemStopBehavior.StopEmitting);
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(3f);
             
             confetti.gameObject.SetActive(false);
         }
