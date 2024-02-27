4be358d3bd4dd65edf685b7c650af42acb375884
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-02-27 18:34:22 +0100

    Update in-task instructions canvas
    Previously the canvas was a child of the simulation game object, which meant that the canvas scaled when the simulation was scaled. Now the scale of the instructions canvas is independent of the scale of the simulation.

---
+++ Client/vr-client/Assets/NarupaIMD/Subtle Game/UI/Canvas/TaskInstructionsManager.cs
@@ -0,0 +1,64 @@
+﻿using NarupaIMD.Subtle_Game.Interaction;
+using NarupaIMD.Subtle_Game.UI.Simulation;
+using UnityEngine;
+
+namespace NarupaIMD.Subtle_Game.Canvas
+{
+    public class TaskInstructionsManager : MonoBehaviour
+    {
+        /// <summary>
+        /// Game object with the instructions on how interact with molecules using hands.
+        /// </summary>
+        [SerializeField] private GameObject handInstructions;
+        
+        /// <summary>
+        /// Game object with the instructions on how interact with molecules using controllers.
+        /// </summary>
+        [SerializeField] private GameObject controllerInstructions;
+
+        /// <summary>
+        /// The pinch grab script.
+        /// </summary>
+        [SerializeField] private PinchGrab pinchGrab;
+        
+        /// <summary>
+        /// The game object representing the center of the XY plane of the simulation box.
+        /// </summary>
+        [SerializeField] private CenterXYPlane centerXYPlane;
+        
+        /// <summary>
+        /// Updates the in-task instructions based on the current interaction modality set in the Pinch Grab script.
+        /// </summary>
+        private void Update()
+        {
+            if (pinchGrab.UseControllers)
+            {
+                handInstructions.SetActive(false);
+                controllerInstructions.SetActive(true);
+            }
+            else
+            {
+                handInstructions.SetActive(true);
+                controllerInstructions.SetActive(false); 
+            }
+        }
+        
+        /// <summary>
+        /// Sets the position and enables the canvas for the in-task instructions.
+        /// </summary>
+        private void OnEnable()
+        {
+            // Update position
+            transform.position = centerXYPlane.transform.position;
+            
+            // Set active
+            gameObject.SetActive(true);
+            
+            // Set background active
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+}
--- Client/vr-client/Assets/NarupaIMD/Subtle Game/UI/Simulation/CenterXYPlane.cs
+++ Client/vr-client/Assets/NarupaIMD/Subtle Game/UI/Simulation/CenterXYPlane.cs
@@ -1,5 +1,5 @@
 using Narupa.Visualisation;
-using NarupaIMD.Subtle_Game.Interaction;
+using NarupaIMD.Subtle_Game.Canvas;
 using UnityEngine;
 
 namespace NarupaIMD.Subtle_Game.UI.Simulation
@@ -10,27 +10,12 @@ namespace NarupaIMD.Subtle_Game.UI.Simulation
         /// The simulation box.
         /// </summary>
         [SerializeField] private BoxVisualiser simulationBox;
+        
+        /// <summary>
+        /// The in-task instructions manager.
+        /// </summary>
+        [SerializeField] private TaskInstructionsManager taskInstructionsManager;
 
-        /// <summary>
-        /// Canvas for the in-task instructions panel.
-        /// </summary>
-        [SerializeField] private GameObject instructionsCanvas;
-        
-        /// <summary>
-        /// Game object with the instructions on how interact with molecules using hands.
-        /// </summary>
-        [SerializeField] private GameObject handInstructions;
-        
-        /// <summary>
-        /// Game object with the instructions on how interact with molecules using controllers.
-        /// </summary>
-        [SerializeField] private GameObject controllerInstructions;
-
-        /// <summary>
-        /// The pinch grab script.
-        /// </summary>
-        [SerializeField] private PinchGrab pinchGrab;
-        
         /// <summary>
         /// Sets the position of this game object to the center of the xy plane of the simulation box and shows the
         /// in-task instructions canvas.
@@ -43,52 +28,16 @@ namespace NarupaIMD.Subtle_Game.UI.Simulation
                 simulationBox.xMagnitude * 0.5f, 
                 simulationBox.xMagnitude);
             
-            // Update instructions canvas
-            ShowInstructionsCanvas();
+            // Show in-task instructions
+            taskInstructionsManager.gameObject.SetActive(true);
         }
         
         /// <summary>
-        /// Updates the in-task instructions based on the current interaction modality set in the Pinch Grab script.
-        /// </summary>
-        private void Update()
-        {
-            // Set in-task instructions based on current interaction modality
-            if (pinchGrab.UseControllers)
-            {
-                handInstructions.SetActive(false);
-                controllerInstructions.SetActive(true);
-            }
-            else
-            {
-                handInstructions.SetActive(true);
-                controllerInstructions.SetActive(false); 
-            }
-        }
-        
-        /// <summary>
-        /// Sets the position and enables the canvas for the in-task instructions.
-        /// </summary>
-        private void ShowInstructionsCanvas()
-        {
-            // Update position
-            instructionsCanvas.transform.position = transform.position;
-            
-            // Set active
-            instructionsCanvas.SetActive(true);
-            
-            // Set background active
-            foreach (Transform child in instructionsCanvas.transform)
-            {
-                child.gameObject.SetActive(true);
-            }
-        }
-        
-        /// <summary>
-        /// Disables the canvas for the in-task instructions when the simulation object is disabled.
+        /// Hides the in-task instructions canvas.
         /// </summary>
         private void OnDisable()
         {
-            instructionsCanvas.SetActive(false);
+            taskInstructionsManager.gameObject.SetActive(false);
         }
     }
 }
