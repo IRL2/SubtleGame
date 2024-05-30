62ea9c26e7e19ba4ce8be333b615df081c761b73
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-05-30 16:30:38 +0200

    Update CenterRightFace.cs
    Update documentation and clean up code.

--- Client/vr-client/Assets/NanoverIMD/Subtle Game/UI/Simulation/CenterRightFace.cs
+++ Client/vr-client/Assets/NanoverIMD/Subtle Game/UI/Simulation/CenterRightFace.cs
@@ -9,21 +9,17 @@ namespace NanoverImd.Subtle_Game.UI.Simulation
         private SynchronisedFrameSource frameSource;
 
         /// <summary>
-        /// Sets the position of this game object to the center of the xy plane of the simulation box and shows the
-        /// in-task instructions canvas.
+        /// Sets the position of this game object to the center of the right hand side of the simulation box.
         /// </summary>
         public void Update()
         {
             // Update position of current game object
-            if (frameSource.CurrentFrame is { } frame
-             && frame.BoxVectors is { } box)
-            {
-                transform.localPosition = new Vector3(
-                    box.axesMagnitudes.x * 1.0f,
-                    box.axesMagnitudes.x * 0.5f,
-                    box.axesMagnitudes.x * 0.5f);
-                transform.localRotation = Quaternion.LookRotation(Vector3.right);
-            }  
+            if (frameSource.CurrentFrame is not { BoxVectors: { } box }) return;
+            transform.localPosition = new Vector3(
+                box.axesMagnitudes.x * 1.0f,
+                box.axesMagnitudes.x * 0.5f,
+                box.axesMagnitudes.x * 0.5f);
+            transform.localRotation = Quaternion.LookRotation(Vector3.right);
         }
     }
 }
