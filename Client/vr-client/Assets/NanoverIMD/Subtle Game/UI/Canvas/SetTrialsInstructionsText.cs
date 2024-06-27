ceb6550ae3b2e9ca292d1a656d618f4603e0885a
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-06-27 17:25:36 +0200

    Update text on trials UI

---
+++ Client/vr-client/Assets/NanoverIMD/Subtle Game/UI/Canvas/SetTrialsInstructionsText.cs
@@ -0,0 +1,25 @@
+﻿using NanoverImd.Subtle_Game;
+using TMPro;
+using UnityEngine;
+
+
+namespace NanoverIMD.Subtle_Game.UI.Canvas
+{
+    public class SetTrialsInstructionsText : MonoBehaviour
+    {
+        private SubtleGameManager _subtleGameManager;
+        [SerializeField] private TMP_Text text;
+       private string _modality;
+
+       private void OnEnable()
+        {
+            _subtleGameManager = FindObjectOfType<SubtleGameManager>();
+
+            if (_subtleGameManager == null) return;
+
+            _modality = _subtleGameManager.CurrentInteractionModality == SubtleGameManager.Modality.Controllers ? "controller" : "hand";
+            text.SetText(
+                $"1. Interact with the molecules for 15 seconds\n2. Select the softest one by holding your {_modality} inside");
+        }
+    }
+}
