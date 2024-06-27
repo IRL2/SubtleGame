d4e7a448fc66e1cc259616ff3c04143da0ab15bc
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-06-27 17:01:07 +0200

    Added hand tracking videos + choose based on current interaction mode
    Added videos for the trials task for the virtual mudra gloves (hand tracking). The correct video now shows depending on whether the current interaction mode.

--- Client/vr-client/Assets/NanoverIMD/Subtle Game/UI/Canvas/VideoPlayerController.cs
+++ Client/vr-client/Assets/NanoverIMD/Subtle Game/UI/Canvas/VideoPlayerController.cs
@@ -1,13 +1,34 @@
 ﻿using System;
+using NanoverImd.Subtle_Game;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
-namespace NanoverImd.Subtle_Game.Canvas
+namespace NanoverIMD.Subtle_Game.UI.Canvas
 {
     public class VideoPlayerController : MonoBehaviour
     {
         public event Action VideoMenuScreenEnabled;
+
+        [SerializeField] private Texture handsTexture;
+        [SerializeField] private Texture controllersTexture;
+        [SerializeField] private RawImage rawImage;
+
+        private SubtleGameManager _subtleGameManager;
+        
         private void OnEnable()
         {
+            _subtleGameManager = FindObjectOfType<SubtleGameManager>();
+            
+            if (handsTexture != null && controllersTexture != null && rawImage != null && _subtleGameManager != null)
+            {
+                // Set the correct video (hands or controllers)
+                rawImage.texture = _subtleGameManager.CurrentInteractionModality == SubtleGameManager.Modality.Hands
+                    ? handsTexture
+                    : controllersTexture;
+            }
+
+            // Play video
             VideoMenuScreenEnabled?.Invoke();
         }
     }
