b18d95a185bf6bfb0c4885cbd271152c1beb60f9
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-07-04 12:42:43 +0200

    Add training button in sandbox task
    This is a dummy button for practising pressing the UI buttons. When pressed, some text appears and fades.

---
+++ Client/vr-client/Assets/NanoverIMD/Subtle Game/UI/Canvas/ButtonTrainingManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NanoverImd.Subtle_Game;
+using NanoverIMD.Subtle_Game.UI.Simulation;
+using UnityEngine;
+
+namespace NanoverIMD.Subtle_Game.UI.Canvas
+{
+    public class ButtonTrainingManager : MonoBehaviour
+    {
+
+        [SerializeField] private CenterLeftFace centerLeftFace;
+        private SubtleGameManager _subtleGameManager;
+        [SerializeField] private GameObject sidePanel;
+
+        private void Start()
+        {
+            _subtleGameManager = FindObjectOfType<SubtleGameManager>();
+        }
+
+        private void Update()
+        {
+            PlacePanelOnLeftFaceOfSimBox();
+            sidePanel.SetActive(_subtleGameManager.CurrentTaskType == SubtleGameManager.TaskTypeVal.Sandbox);
+        }
+
+
+        private void PlacePanelOnLeftFaceOfSimBox()
+        {
+            gameObject.transform.position = centerLeftFace.transform.position;
+            gameObject.transform.rotation = centerLeftFace.transform.rotation;
+        }
+    }
+}
---
+++ Client/vr-client/Assets/NanoverIMD/Subtle Game/UI/Simulation/CenterLeftFace.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nanover.Visualisation;
+using UnityEngine;
+
+namespace NanoverIMD.Subtle_Game.UI.Simulation
+{
+    public class CenterLeftFace : MonoBehaviour
+    {
+        [SerializeField]
+        private SynchronisedFrameSource frameSource;
+
+        /// <summary>
+        /// Sets the position of this game object to the center of the left hand side of the simulation box.
+        /// </summary>
+        public void Update()
+        {
+            // Update position of current game object
+            if (frameSource.CurrentFrame is not { BoxVectors: { } box }) return;
+            
+            // Don't reposition if the box lengths are zero
+            if (Math.Abs(box.axesMagnitudes.x) <= 0.001f) return;
+            
+            // Place the current game object at the center of the left hand side face of the sim box
+            transform.localPosition = new Vector3(
+                box.axesMagnitudes.x * 0f,
+                box.axesMagnitudes.x * 0.5f,
+                box.axesMagnitudes.x * 0.5f);
+            transform.localRotation = Quaternion.LookRotation(Vector3.left);
+        }
+    }
+}
