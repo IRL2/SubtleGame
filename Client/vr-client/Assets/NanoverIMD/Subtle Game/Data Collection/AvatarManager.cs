cba551e99b2a8521dda13f531e9cb523c4ffbae4
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-08-14 13:55:06 +0200

    First attempt at correcting avatar position
    Not working yet.

--- Client/vr-client/Assets/NanoverIMD/Subtle Game/Data Collection/AvatarManager.cs
+++ Client/vr-client/Assets/NanoverIMD/Subtle Game/Data Collection/AvatarManager.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Nanover.Core.Math;
 using Nanover.Frontend.Utility;
 using Nanover.Grpc.Multiplayer;
+using NanoverIMD.Subtle_Game;
 using Oculus.Interaction.Input;
 using UnityEngine;
 
@@ -37,6 +38,9 @@ namespace NanoverImd.Subtle_Game.Data_Collection
         private Coroutine sendAvatarsCoroutine;
 
         [SerializeField] private Transform avatarsParentTransform;
+
+        private Matrix4x4 _simMatrixFromServerInverse = Matrix4x4.zero;
+        private Matrix4x4 _simMatrixInUnity = Matrix4x4.zero;
 
         /// <summary>
         /// Updates the local avatar data and flushes this to the shared state.
@@ -108,19 +112,35 @@ namespace NanoverImd.Subtle_Game.Data_Collection
                 .Avatars.OtherPlayerAvatars
                 .SelectMany(avatar => avatar.Components, (avatar, component) =>
                     (Avatar: avatar, Component: component))
-                .Where(res => res.Component.Name == MultiplayerAvatar.LeftHandName
-                              || res.Component.Name == MultiplayerAvatar.RightHandName);
+                .Where(res => res.Component.Name is MultiplayerAvatar.LeftHandName or MultiplayerAvatar.RightHandName);
 
             headsetObjects.MapConfig(headsets, UpdateAvatarComponent);
             controllerObjects.MapConfig(controllers, UpdateAvatarComponent);
 
             void UpdateAvatarComponent((MultiplayerAvatar Avatar, MultiplayerAvatar.Component Component) value, AvatarModel model)
             {
-                var transformation = value.Component.Transformation;
-                model.transform.SetPositionAndRotation(transformation.position, transformation.rotation);
+                // Get box transformation matrix in server space
+                var boxTransformation = simulation.Multiplayer.SimulationPose;
+                
+                // Calculate transformation matrix that takes the box from server to game space
+                _simMatrixFromServerInverse = boxTransformation.Value.InverseMatrix;
+                _simMatrixInUnity = simulation.transform.localToWorldMatrix;
+                var conversionMatrix = _simMatrixInUnity * _simMatrixFromServerInverse;
+
+                // Save avatar transformation matrix in server space
+                var avatarMatrix = value.Component.Transformation.matrix;
+
+                // Multiply avatar matrix by conversion matrix
+                var transformedMatrix = conversionMatrix * avatarMatrix;
+
+                // Set the transformed position and rotation of the avatar model
+                 model.transform.SetPositionAndRotation(transformedMatrix.GetPosition(), transformedMatrix.rotation);
+
+                 // Apply additional properties to the model
                 model.SetPlayerColor(value.Avatar.Color);
                 model.SetPlayerName(value.Avatar.Name);
             }
+
         }
     }
 }
