163f2eb090aec90ecd34e33152742fbf96018b86
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-08-27 10:44:28 +0200

    Optimise code for renderer managers
    Created a base class that both managers inherit from.

--- Client/vr-client/Assets/NanoverIMD/Subtle Game/Interaction/AtomMarkerManager.cs
+++ Client/vr-client/Assets/NanoverIMD/Subtle Game/Interaction/AtomMarkerManager.cs
@@ -1,77 +1,21 @@
-﻿using Nanover.Frontend.Utility;
-using NanoverImd;
-using NanoverImd.Interaction;
+﻿using NanoverImd.Interaction;
 using UnityEngine;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace NanoverIMD.Subtle_Game.Interaction
 {
-    public class AtomMarkerManager : MonoBehaviour
+    /// <summary>
+    /// Handles the rendering of the atom markers that show which atom the player is nearest to or is currently
+    /// interacting with.
+    /// </summary>
+    public class AtomMarkerManager : RendererManager<AtomMarkerRenderer>
     {
-        [SerializeField] private NanoverImdSimulation simulation;
-        [SerializeField] private AtomMarkerRenderer atomMarkerRendererTemplate;
-        private IndexedPool<AtomMarkerRenderer> _rendererPool;
-        
-        private void Start()
-        {
-            _rendererPool = new IndexedPool<AtomMarkerRenderer>(CreateInstanceCallback, ActivateInstanceCallback,
-                DeactivateInstanceCallback);
-        }
-        
         /// <summary>
-        /// Deactivate the interaction renderer.
+        /// Update the values for the renderer associated with this interaction.
         /// </summary>
-        private void DeactivateInstanceCallback(AtomMarkerRenderer obj)
+        protected override void ConfigureRenderer(ParticleInteraction interaction, AtomMarkerRenderer rendererInstance, Vector3 particlePositionWorld)
         {
-            obj.gameObject.SetActive(false);
-        }
-        
-        /// <summary>
-        /// Activate the interaction renderer.
-        /// </summary>
-        private void ActivateInstanceCallback(AtomMarkerRenderer obj)
-        {
-            obj.gameObject.SetActive(true);
-        }
-        
-        /// <summary>
-        /// Create an instance of the interaction renderer, set its parent to the current transform, and set active.
-        /// </summary>
-        private AtomMarkerRenderer CreateInstanceCallback()
-        {
-            var atomMarkerRenderer = Instantiate(atomMarkerRendererTemplate, transform, true);
-            atomMarkerRenderer.gameObject.SetActive(true);
-            return atomMarkerRenderer;
-        }
-        
-        // TODO: Ensure interactions with a force scale of 0 are not rendered.
-        /// <summary>
-        /// Set the atom marker rendering for all known interactions.
-        /// </summary>
-        private void Update()
-        {
-            var interactions = simulation.Interactions;
-            var frame = simulation.FrameSynchronizer.CurrentFrame;
-            
-            _rendererPool.MapConfig(interactions.Values, MapConfigToInstance);
-
-            void MapConfigToInstance(ParticleInteraction interaction, 
-                AtomMarkerRenderer markerRenderer)
-            {
-                var particlePositionSim = ComputeParticleCentroid(interaction.Particles);
-                var particlePositionWorld = transform.TransformPoint(particlePositionSim);
-                
-                markerRenderer.scale = interaction.Scale;
-                markerRenderer.ParticlePosition = particlePositionWorld;
-            }
-
-            Vector3 ComputeParticleCentroid(IReadOnlyList<int> particleIds)
-            {
-                var centroid = particleIds.Aggregate(Vector3.zero, (current, t) => current + frame.ParticlePositions[t]);
-
-                return centroid / particleIds.Count;
-            }
+            rendererInstance.scale = interaction.Scale;
+            rendererInstance.ParticlePosition = particlePositionWorld;
         }
     }
 }
--- Client/vr-client/Assets/NanoverIMD/Subtle Game/Interaction/InteractionRendererManager.cs
+++ Client/vr-client/Assets/NanoverIMD/Subtle Game/Interaction/InteractionRendererManager.cs
@@ -1,80 +1,21 @@
-﻿using Nanover.Frontend.Utility;
-using NanoverImd;
-using NanoverImd.Interaction;
+﻿using NanoverImd.Interaction;
 using UnityEngine;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace NanoverIMD.Subtle_Game.Interaction
 {
     /// <summary>
-    /// Manage instances of InteractionRenderer so that all known interactions are rendered.
+    /// Handles the rendering of the interaction lines of all known interactions.
     /// </summary>
-    public class InteractionRendererManager : MonoBehaviour
+    public class InteractionRendererManager : RendererManager<InteractionRenderer>
     {
-        [SerializeField] private NanoverImdSimulation simulation;
-        [SerializeField] private InteractionRenderer interactionRendererTemplate;
-        private IndexedPool<InteractionRenderer> _rendererPool;
-
-        private void Start()
-        {
-            _rendererPool = new IndexedPool<InteractionRenderer>(CreateInstanceCallback, ActivateInstanceCallback,
-                DeactivateInstanceCallback);
-        }
-        
         /// <summary>
-        /// Deactivate the interaction renderer.
+        /// Update the values for the renderer associated with this interaction.
         /// </summary>
-        private void DeactivateInstanceCallback(InteractionRenderer obj)
-        {
-            obj.gameObject.SetActive(false);
-        }
-        
-        /// <summary>
-        /// Activate the interaction renderer.
-        /// </summary>
-        private void ActivateInstanceCallback(InteractionRenderer obj)
-        {
-            obj.gameObject.SetActive(true);
-        }
-        
-        /// <summary>
-        /// Create an instance of the interaction renderer, set its parent to the current transform, and set active.
-        /// </summary>
-        private InteractionRenderer CreateInstanceCallback()
-        {
-            var interactionRenderer = Instantiate(interactionRendererTemplate, transform, true);
-            interactionRenderer.gameObject.SetActive(true);
-            return interactionRenderer;
-        }
-        
-        // TODO: Ensure interactions with a force scale of 0 are not rendered.
-        /// <summary>
-        /// Set the rendering for all known interactions.
-        /// </summary>
-        private void Update()
-        {
-            var interactions = simulation.Interactions;
-            var frame = simulation.FrameSynchronizer.CurrentFrame;
-            _rendererPool.MapConfig(interactions.Values, MapConfigToInstance);
-
-            void MapConfigToInstance(ParticleInteraction interaction, 
-                InteractionRenderer interactionRenderer)
+        protected override void ConfigureRenderer(ParticleInteraction interaction, InteractionRenderer rendererInstance, Vector3 particlePositionWorld)
             {
-                var particlePositionSim = ComputeParticleCentroid(interaction.Particles);
-                var particlePositionWorld = transform.TransformPoint(particlePositionSim);
-
-                interactionRenderer.scale = interaction.Scale;
-                interactionRenderer.EndPosition = transform.TransformPoint(interaction.Position);
-                interactionRenderer.StartPosition = particlePositionWorld;
+                rendererInstance.scale = interaction.Scale;
+                rendererInstance.EndPosition = transform.TransformPoint(interaction.Position);
+                rendererInstance.StartPosition = particlePositionWorld;
             }
-
-            Vector3 ComputeParticleCentroid(IReadOnlyList<int> particleIds)
-            {
-                var centroid = particleIds.Aggregate(Vector3.zero, (current, t) => current + frame.ParticlePositions[t]);
-
-                return centroid / particleIds.Count;
-            }
-        }
     }
 }
