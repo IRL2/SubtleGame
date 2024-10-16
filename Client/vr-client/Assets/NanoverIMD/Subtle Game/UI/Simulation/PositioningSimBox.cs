c436f146646c82c26e10a8d6f5129262e08d6da8
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2024-10-16 12:16:45 +0200

    checkpoint after session with Harry and Mark

--- Client/vr-client/Assets/NanoverIMD/Subtle Game/UI/Simulation/PositioningSimBox.cs
+++ Client/vr-client/Assets/NanoverIMD/Subtle Game/UI/Simulation/PositioningSimBox.cs
@@ -1,9 +1,14 @@
-﻿using Nanover.Core.Math;
+﻿using System.Numerics;
+using Nanover.Core.Math;
 using Nanover.Visualisation;
 using NanoverImd;
 using NanoverImd.Subtle_Game;
 using NanoverIMD.Subtle_Game.Data_Collection;
 using UnityEngine;
+using Matrix4x4 = UnityEngine.Matrix4x4;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+using Vector4 = UnityEngine.Vector4;
 
 namespace NanoverIMD.Subtle_Game.UI.Simulation
 {
@@ -13,6 +18,7 @@ namespace NanoverIMD.Subtle_Game.UI.Simulation
         private NanoverImdApplication application;
 
         [SerializeField] private Transform boxCenter;
+        [SerializeField] private Transform box;
 
         private Vector3 desiredPosition;
         private Quaternion desiredRotation;
@@ -62,7 +68,17 @@ namespace NanoverIMD.Subtle_Game.UI.Simulation
         };
 
         private void Start()
-            {
+        {
+            var size = Vector3.one * 7f;
+            var m = Matrix4x4.TRS(size * .5f, Quaternion.identity, Vector3.one);
+            Debug.LogError(m * new Vector4(0, 0, 0, 1f));
+            
+                //Debug.LogError(Matrix4x4.TRS(Vector3.one * 7f, Quaternion.identity, Vector3.one));
+                var g = new GameObject("bla");
+                g.transform.position = Vector3.one * 7f;
+                //Debug.LogError(g.transform.localToWorldMatrix);
+                //Debug.LogError(g.transform.worldToLocalMatrix);
+                
                 var obj = new GameObject("REFERENCE");
                 playerReference = obj.transform;
                 playerReference.SetParent(transform);
@@ -75,6 +91,7 @@ namespace NanoverIMD.Subtle_Game.UI.Simulation
                 CheckTaskChanged();
                 CheckBoxSizeChanged();
                 
+                UpdatePlayerPosition();
                 UpdatePose();
 
                 if (_taskChanged) UpdateOffsets();
@@ -91,7 +108,7 @@ namespace NanoverIMD.Subtle_Game.UI.Simulation
                     _boxSizeChanged = _taskChanged = false;
                 }
 
-                // UpdatePose();
+                //UpdatePose();
             }
 
             private void UpdatePose()
@@ -100,37 +117,59 @@ namespace NanoverIMD.Subtle_Game.UI.Simulation
                 // TODO: Change this code to move the calibrated space
                 
                 // Specify rotation
-                desiredRotation = Quaternion.LookRotation(-playerReference.forward);
+                //desiredRotation = Quaternion.LookRotation(-playerReference.forward);
                 
                 // Specify position
-                desiredPosition = new Vector3(
-                    playerReference.position.x + boxCenter.position.x, 
-                    -(playerReference.position.y + boxCenter.position.y), 
-                    playerReference.position.z + boxCenter.position.z);
-                
-                Debug.LogWarning(boxCenter.position);
+                //desiredPosition = new Vector3(
+                //    playerReference.position.x + boxCenter.position.x, 
+                //    -(playerReference.position.y + boxCenter.position.y), 
+                //    playerReference.position.z + boxCenter.position.z);
 
                 // Create the transformation
-                var desiredTransformation = new Transformation(desiredPosition, desiredRotation, Vector3.one);
+                //var desiredTransformation = new Transformation(desiredPosition, desiredRotation, Vector3.one);
                 
                 // Convert transformation from world into calibrated space
-                var calibratedPose = application.CalibratedSpace.TransformPoseWorldToCalibrated(desiredTransformation);
+                //var calibratedPose = application.CalibratedSpace.TransformPoseWorldToCalibrated(desiredTransformation);
                 
                 // Create transformation matrix
-                var transformationMatrix = Matrix4x4.TRS(calibratedPose.Position, calibratedPose.Rotation, Vector3.one);
+                // var transformationMatrix = Matrix4x4.TRS(calibratedPose.Position, calibratedPose.Rotation, Vector3.one);
+                
+                var halfBoxSize = _currentBoxSize / 2;
+                var boxCenterMatrix = Matrix4x4.TRS(new Vector3(-halfBoxSize, halfBoxSize, halfBoxSize), Quaternion.identity, Vector3.one);
+
+                var rot180 = Quaternion.AngleAxis(180f * (Time.time % 1f), Vector3.up);
+                rot180 = Quaternion.AngleAxis(180f, Vector3.up);
+                var rotation = Matrix4x4.TRS(Vector3.zero, rot180, Vector3.one);
+                //var vectorToBoxCenter = boxCenter.position - box.position;
+                var InvHeadset = playerReference.localToWorldMatrix;
+                
+                //var p = Matrix4x4.TRS(-vectorToBoxCenter, Quaternion.identity, Vector3.one);
+                //p = Matrix4x4.identity;
+
+                
                 
                 // Calibrate the space
-                application.CalibratedSpace.CalibrateFromMatrix(transformationMatrix);
+                application.CalibratedSpace.CalibrateFromMatrix(InvHeadset * boxCenterMatrix.inverse);
+
+                return;
                 
-                // set teh scale
-                if (subtleGameManager.simulation.Multiplayer.IsOpen)
-                {
-                    var currentPose = subtleGameManager.simulation.Multiplayer.SimulationPose.Value;
-                    currentPose.Scale = Vector3.one * Scale;
-                    subtleGameManager.simulation.Multiplayer.SimulationPose.UpdateValueWithLock(currentPose);
-                }
+                // we are in the center of the box at the correct scale
+                // the box needs to be rotated 180
+                // but rotation as the last step seems to have no effect??? -- next time
                 
-                /*// box scale
+                // Don't set the scale when we are replaying simulations
+                if (subtleGameManager.CurrentTaskType is SubtleGameManager.TaskTypeVal.TrialsObserver
+                        or SubtleGameManager.TaskTypeVal.TrialsObserverTraining)
+                    return;
+                
+                // Set the scale
+                if (!subtleGameManager.simulation.Multiplayer.IsOpen) return;
+                var currentPose = subtleGameManager.simulation.Multiplayer.SimulationPose.Value;
+                currentPose.Scale = Vector3.one * Scale;
+                subtleGameManager.simulation.Multiplayer.SimulationPose.UpdateValueWithLock(currentPose);
+
+                /*
+                // box scale
                 var scale = Scale;
 
                 // half box size in world coordinates
@@ -153,7 +192,8 @@ namespace NanoverIMD.Subtle_Game.UI.Simulation
                 if (subtleGameManager.simulation.Multiplayer.IsOpen)
                 {
                     subtleGameManager.simulation.Multiplayer.SimulationPose.UpdateValueWithLock(t);
-                }*/
+                }
+                */
             }
 
             private void UpdatePlayerPosition()
@@ -163,6 +203,8 @@ namespace NanoverIMD.Subtle_Game.UI.Simulation
                 forward.Normalize();
                 playerReference.position = centreEyeAnchor.position;
                 playerReference.rotation = Quaternion.LookRotation(forward);
+                
+                // playerReference.rotation = centreEyeAnchor.rotation;
             }
             
             private void CheckTaskChanged()
