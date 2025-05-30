c0db59c1482a3faa23c82dd77cfade99ec9b69fc
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2025-05-30 19:10:16 +0200

    Update text on Observer trials menu
    Change "you only have 30 seconds to interact" to "you only have 30 seconds to watch".

---
+++ Client/vr-client/Assets/NanoverIMD/Subtle Game/UI/Canvas/SetObserverTrialsMenu3Text.cs
@@ -0,0 +1,19 @@
+﻿using NanoverImd.Subtle_Game;
+using TMPro;
+using UnityEngine;
+
+namespace NanoverIMD.Subtle_Game.UI.Canvas
+{
+    public class SetObserverTrialsMenu3Text: MonoBehaviour
+    {
+        [SerializeField] private TMP_Text text;
+        private string _timeLimit;
+
+        private void OnEnable()
+        {
+            _timeLimit = PlayerPrefs.GetFloat(SubtleGameManager.TrialTimeLimit).ToString();
+            
+            text.SetText($"Your progress will be shown on a panel to your right\n\nRemember, you will only have {_timeLimit} seconds to watch!");
+        }
+    }
+}
